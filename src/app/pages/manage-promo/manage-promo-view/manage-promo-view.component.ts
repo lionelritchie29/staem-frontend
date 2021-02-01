d92af72078673c76633a55fbd6485606fd1a50fd
lionelritchie29 <lionelritchie29@github.com>
2021-02-01 10:04:37 +0700

    Added pagination for manage user and manage promo

--- src/app/pages/manage-promo/manage-promo-view/manage-promo-view.component.ts
+++ src/app/pages/manage-promo/manage-promo-view/manage-promo-view.component.ts
@@ -7,17 +7,48 @@ import { GameSale } from 'src/app/models/game-sale';
 import { AllSpecialOffersGqlService } from 'src/app/services/gql/query/all-special-offers-gql.service';
 
 const DELETE_PROMO = gql`
-  mutation deleteSale($gameId:Int) {
-    deletePromo(gameId:$gameId)
+  mutation deleteSale($gameId: Int) {
+    deletePromo(gameId: $gameId)
   }
 `;
 
-const UPDATE_PROMO = gql`
-  mutation updateSale($gameId:Int, $disc:Int, $validTo:String){
-    updatePromo(gameId:$gameId, discount: $disc,validTo:$validTo){
-      gameId,
-      validTo,
+const SALE_LIMIT_OFFSET = gql`
+  query getGameOnSaleLimitOffset($limit: Int, $offset: Int) {
+    gamePromoLimitOffset(limit: $limit, offset: $offset) {
+      totalCount
+      games {
+        id
+        title
+        description
+        price
+        releasedate
+        developer {
+          id
+          name
+        }
+        publisher {
+          id
+          name
+        }
+        tags {
+          id
+          name
+          isAdult
+        }
+        genres {
+          id
+          name
+        }
+        images {
+          url
+        }
+      }
+    }
+
+    gameSales {
+      gameId
       discount
+      validTo
     }
   }
 `;
@@ -25,46 +56,74 @@ const UPDATE_PROMO = gql`
 @Component({
   selector: 'app-manage-promo-view',
   templateUrl: './manage-promo-view.component.html',
-  styleUrls: ['./manage-promo-view.component.scss']
+  styleUrls: ['./manage-promo-view.component.scss'],
 })
 export class ManagePromoViewComponent implements OnInit {
-
   gameList: Game[] = [];
   sales: GameSale[] = [];
 
-  constructor(
-    private AllSpecialOffersGqlService: AllSpecialOffersGqlService,
-    private apollo: Apollo,
-    private router: Router
-  ) { }
+  limit: number = 5;
+  offset: number = 0;
+  page: number = 1;
+  totalPage: number = 1;
+
+  constructor(private apollo: Apollo, private router: Router) {}
 
   ngOnInit(): void {
-    this.AllSpecialOffersGqlService.watch()
-      .valueChanges
-      .pipe(map(res => res.data))
-      .subscribe(res => {
-        this.gameList = res.gamesOnSale;
+    this.fetchGames();
+  }
+
+  fetchGames() {
+    this.apollo
+      .watchQuery({
+        query: SALE_LIMIT_OFFSET,
+        variables: { limit: this.limit, offset: this.offset },
+      })
+      .valueChanges.pipe(map((res) => <any>res.data))
+      .subscribe((res) => {
+        console.log(res);
+        this.totalPage = Math.ceil(
+          res.gamePromoLimitOffset.totalCount / this.limit
+        );
+        this.gameList = res.gamePromoLimitOffset.games;
         this.sales = res.gameSales;
       });
   }
 
   onDelete(gameId: number) {
-    this.apollo.mutate({
-      mutation: DELETE_PROMO,
-      variables: {gameId},
-    }).pipe(map(res => (<any>res.data).deletePromo))
-    .subscribe(isSuccess => {
-      if (isSuccess) {
-        alert("Delete Success");
-        window.location.reload();
-      } else {
-        alert("Delete Failed");
-      }
-    })
+    this.apollo
+      .mutate({
+        mutation: DELETE_PROMO,
+        variables: { gameId },
+      })
+      .pipe(map((res) => (<any>res.data).deletePromo))
+      .subscribe((isSuccess) => {
+        if (isSuccess) {
+          alert('Delete Success');
+          window.location.reload();
+        } else {
+          alert('Delete Failed');
+        }
+      });
   }
 
   onUpdate(gameId: number) {
-    this.router.navigate([`/admin/manage-promo/update/${gameId}`])
+    this.router.navigate([`/admin/manage-promo/update/${gameId}`]);
   }
 
+  onNext() {
+    if (this.page == this.totalPage) return;
+
+    this.page += 1;
+    this.offset = (this.page - 1) * this.limit;
+    this.fetchGames();
+  }
+
+  onPrev() {
+    if (this.page == 1) return;
+
+    this.page -= 1;
+    this.offset = (this.page - 1) * this.limit;
+    this.fetchGames();
+  }
 }
--- src/app/pages/manage-user/manage-user-view/manage-user-view.component.ts
+++ src/app/pages/manage-user/manage-user-view/manage-user-view.component.ts
@@ -1,28 +1,92 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 import { UserAccount } from 'src/app/models/user-account';
 import { UserReport } from 'src/app/models/user-report';
 import { AllUsersGqlService } from 'src/app/services/gql/query/all-users-gql.service';
 
+const GET_USERS_LIMIT_OFFSET = gql`
+  query getUserLimitOffset($limit: Int, $offset: Int) {
+    usersLimitOffset(limit: $limit, offset: $offset) {
+      users {
+        id
+        accountName
+        code
+        email
+        status
+        suspendedAt
+        walletAmount
+        role {
+          id
+          name
+        }
+        profile {
+          avatarFrameUrl
+          country
+          customURL
+          displayName
+          featuredBadgeUrl
+          level
+          miniProfileBackgroundUrl
+          point
+          profileBackgroundUrl
+          profilePictureUrl
+          realName
+          summary
+          theme
+        }
+      }
+      totalCount
+    }
+  }
+`;
+
 @Component({
   selector: 'app-manage-user-view',
   templateUrl: './manage-user-view.component.html',
-  styleUrls: ['./manage-user-view.component.scss']
+  styleUrls: ['./manage-user-view.component.scss'],
 })
 export class ManageUserViewComponent implements OnInit {
-
   users: UserAccount[] = [];
   reports: UserReport[] = [];
 
-  constructor(
-    private allUsersGqlService: AllUsersGqlService,
-  ) { }
+  limit: number = 5;
+  offset: number = 0;
+  page: number = 1;
+  totalPage: number = 1;
+
+  constructor(private apollo: Apollo) {}
 
   ngOnInit(): void {
-    this.allUsersGqlService.watch()
-    .valueChanges
-    .subscribe(res => {
-      this.users = res.data.users;
-    })
+    this.fetchUsers();
   }
 
+  fetchUsers() {
+    this.apollo
+      .watchQuery({
+        query: GET_USERS_LIMIT_OFFSET,
+        variables: { limit: this.limit, offset: this.offset },
+      })
+      .valueChanges.subscribe((res) => {
+        this.users = (<any>res.data).usersLimitOffset.users;
+        this.totalPage = Math.ceil(
+          (<any>res.data).usersLimitOffset.totalCount / this.limit
+        );
+      });
+  }
+
+  onNext() {
+    if (this.page == this.totalPage) return;
+
+    this.page += 1;
+    this.offset = (this.page - 1) * this.limit;
+    this.fetchUsers();
+  }
+
+  onPrev() {
+    if (this.page == 1) return;
+
+    this.page -= 1;
+    this.offset = (this.page - 1) * this.limit;
+    this.fetchUsers();
+  }
 }
