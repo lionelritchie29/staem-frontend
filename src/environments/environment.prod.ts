0e6964342f423da0eb7187370a0a7a2e53179d4f
lionelritchie29 <lionelritchie29@github.com>
2021-02-06 15:17:25 +0700

    changed base backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  BASE_URL: 'https://still-beyond-30142.herokuapp.com',
-  WEBSOCKET_URL: 'wss://still-beyond-30142.herokuapp.com/subscriptions',
+  BASE_URL: 'https://staem.herokuapp.com/',
+  WEBSOCKET_URL: 'wss://staem.herokuapp.com/subscriptions',
 };
