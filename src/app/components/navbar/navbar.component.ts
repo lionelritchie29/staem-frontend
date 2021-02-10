494f8db2a278a83b965b29dbc1da34cdc96ebd6b
lionelritchie29 <lionelritchie29@github.com>
2021-02-10 12:38:59 +0700

    added mobile navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,12 +8,12 @@ import { GetUserByIdGqlService } from 'src/app/services/gql/get-user-by-id-gql.s
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
   loggedUser: UserAccount;
   loggedUserImgUrl: string;
+  navMobileActive: boolean = false;
 
   languageList: String[] = [
     '简体中文 (Simplified Chinese)',
@@ -30,12 +30,12 @@ export class NavbarComponent implements OnInit {
     'Norsk (Norwegian)',
     'Português (Portuguese)',
     'Русский (Russian)',
-  ]
+  ];
 
   constructor(
     private getUserByIdGqlService: GetUserByIdGqlService,
     private authService: AuthService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     const loggedUserId = this.authService.getLoggedInUserId();
@@ -44,20 +44,22 @@ export class NavbarComponent implements OnInit {
       this.loggedUser = null;
     } else {
       this.getUserByIdGqlService
-        .watch({id: loggedUserId})
-        .valueChanges
-        .pipe(map(res => res.data.user))
-        .subscribe(user => {
-          this.loggedUser = user
-          this.loggedUserImgUrl = 
-            getUserImageUrl(this.loggedUser.profile.profilePictureUrl);
+        .watch({ id: loggedUserId })
+        .valueChanges.pipe(map((res) => res.data.user))
+        .subscribe((user) => {
+          this.loggedUser = user;
+          this.loggedUserImgUrl = getUserImageUrl(
+            this.loggedUser.profile.profilePictureUrl
+          );
         });
     }
   }
 
   showLanguageList(): void {
-    const languageListElm: HTMLElement = document.getElementById('language-list');
-    
+    const languageListElm: HTMLElement = document.getElementById(
+      'language-list'
+    );
+
     if (languageListElm.style.display === 'block') {
       languageListElm.style.display = 'none';
     } else {
@@ -67,7 +69,7 @@ export class NavbarComponent implements OnInit {
 
   showUserMenuList(): void {
     const userMenuElm: HTMLElement = document.getElementById('user-menu');
-    
+
     if (userMenuElm.style.display === 'block') {
       userMenuElm.style.display = 'none';
     } else {
@@ -79,5 +81,4 @@ export class NavbarComponent implements OnInit {
     this.authService.logout();
     window.location.reload();
   }
-
 }
